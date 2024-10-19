98b5cfa4098377a0b0152ec264750b3dc90bb160
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-19 10:37:08 +0100

    feat: add python data types concept

---
+++ 00-beginner/day-02/README.md
@@ -0,0 +1,31 @@
+# Day 2 - Understanding Data types and How to Manipulate Strings
+
+Today, we covered the following topics:
+
+1. **Data Types**:
+   - Integers
+   - Floats
+   - Strings
+   - Booleans
+
+2. **Numbers and Operations**:
+   - Basic arithmetic operations
+   - Order of operations (PEMDAS)
+
+3. **Type Conversion**:
+   - Converting between different data types
+
+4. **f-Strings**:
+   - Using f-strings for string interpolation
+
+We also completed several exercises to practice these concepts.
+
+## Concepts
+
+- [Primitive Data types]()
+
+## Main Project
+
+### Tip Calculator
+
+
---
+++ 00-beginner/day-02/concepts/00_data-types.py
@@ -0,0 +1,23 @@
+# Primitive Data types
+
+# String
+print("Hello"[0])
+# numbers within quotation marks are treated as strings not numbers
+print("123" + "345") #concatenates instead of add
+
+# Integers
+# Integer is a programming lingo for whole numbers(numbers without decimal places)
+print(123 + 345)
+# You can seperate large integers with underscore e.g
+print(123_456_789)
+
+# Float
+# Float is short for floating-point numbers(decimal numbers). E.g
+float_num = 3.14159 # PI is a floating point number
+print(type(float_num))
+
+# Boolean
+# This data type has only two possible values(True or False)
+checked = True
+print(type(checked)) # <class 'bool'>
+
