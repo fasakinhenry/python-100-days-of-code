37562893cc6c75be646f8fe7eaa4c9c150294e4a
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-22 17:18:21 +0100

    feat: add spacing to input in tip calculator

--- 00-beginner/day-02/tip-calculator.py
+++ 00-beginner/day-02/tip-calculator.py
@@ -8,8 +8,8 @@
 #Write your code below this line 👇
 print("Welcome to the tip calculator!")
 total_bill = float(input("What was the total bill? $"))
-tip = int(input("How much tip would you like to give? 10, 12, or 15?"))
-people = int(input("How many people to split the bill?"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
 
 individual_pay = round((total_bill / people) * (1 + tip / 100), 2)
 print(f"Each person should pay: ${individual_pay}")
