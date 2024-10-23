9d1293362dcaebc54887c9f147b614a8f2aacf0a
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-23 17:05:38 +0100

    feat: add odd or even exercise solution

---
+++ 00-beginner/day-03/concepts/01_odd-or-even.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+
+# Check using the bitwise and operator
+
+if (number | 1) == number:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
