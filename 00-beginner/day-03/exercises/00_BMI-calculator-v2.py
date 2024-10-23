50672688256d8604bee6a76ad543a3c8d514eb77
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-23 22:21:09 +0100

    chore: cleanup bmi calculator

--- 00-beginner/day-03/exercises/00_BMI-calculator-v2.py
+++ 00-beginner/day-03/exercises/00_BMI-calculator-v2.py
@@ -4,4 +4,15 @@ weight = float(input("enter your weight in kg: "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-bmi = weight / height ** 2
+bmi = round(weight / height ** 2)
+
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
