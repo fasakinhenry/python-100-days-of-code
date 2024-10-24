41d209af24b2ad6650112abf1d7068afb9dc0664
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-24 11:11:46 +0100

    feat: add multiple if statements

---
+++ 00-beginner/day-03/concepts/02_multiple-if-statements.py
@@ -0,0 +1,27 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height? "))
+
+# add bill variable to store the bill amount
+bill = 0
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    # Introduce nested if
+    age = int(input("What is your age? "))
+    if age < 12:
+        bill = 5
+        print("Please pay $5.")
+    elif age <= 18:
+        bill = 7
+        print("Please pay $7.")
+    else:
+        bill = 12
+        print("Please pay $12.")
+    
+    # Check if they want photo taken
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        bill += 3
+
+else:
+    print("Sorry, you have to grow taller before you can ride.")
