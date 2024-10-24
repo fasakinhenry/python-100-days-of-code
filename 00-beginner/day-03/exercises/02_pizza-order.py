ab2d3bf502827b4413ad8a99e209212d49249b00
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-24 12:11:34 +0100

    feat: add the code for pizza order

--- 00-beginner/day-03/exercises/02_pizza-order.py
+++ 00-beginner/day-03/exercises/02_pizza-order.py
@@ -6,3 +6,25 @@ extra_cheese = input("Do you want extra cheese? Y or N ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+bill = 0
+
+# Check for the user input of pizza and set bill accordingly
+if size == "S":
+    bill = 15
+elif bill == "M":
+    bill = 20
+elif bill == "L":
+    bill = 25
+
+# Check if they want to add pepperoni
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+# Check if they want to add cheese
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is: ${bill}")
