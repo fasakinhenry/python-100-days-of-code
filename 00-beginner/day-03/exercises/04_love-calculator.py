2f588ba5dd53a01ae70344d87b3eea98527e6447
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-24 15:36:02 +0100

    feat: add the initial files for love calculator

---
+++ 00-beginner/day-03/exercises/04_love-calculator.py
@@ -0,0 +1,7 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
