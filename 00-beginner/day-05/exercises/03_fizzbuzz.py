562666a373106e8758ae69c55ebe61e9b60378bf
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-25 13:43:39 +0100

    feat: add the code for fizzbuzz in day 5

---
+++ 00-beginner/day-05/exercises/03_fizzbuzz.py
@@ -0,0 +1,11 @@
+# Implement the fizzbuzz in python
+# Don't help me
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
