bb31f6aca1ede12336f09905472e30bb93a41162
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-25 09:39:07 +0100

    feat: add code for the for loop

---
+++ 00-beginner/day-05/concepts/00_for-loop.py
@@ -0,0 +1,8 @@
+# Create a list of fruits
+fruits = ["Apple", "Peach", "Pear"]
+
+# Loop through the list of fruits
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
+print("fruits")
