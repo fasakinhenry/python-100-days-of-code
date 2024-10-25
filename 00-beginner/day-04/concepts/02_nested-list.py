606a3b2ffcb425f780aecc05782788fff47fc417
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-25 08:32:37 +0100

    feat: add code for dirty dozens in nested list

---
+++ 00-beginner/day-04/concepts/02_nested-list.py
@@ -0,0 +1,8 @@
+# Get the dirty dozen for fruits and vegetables
+fruits = ['Strawberries', 'Apples', 'Grapes', 'Peaches', 'Pears', 'Cherries', 'Oranges', 'Bananas', 'Pineapples', 'Mangoes', 'Watermelons', 'Blueberries']
+
+vegetables = ['Spinach', 'Kale', 'Carrots', 'Celery', 'Beets', 'Broccoli', 'Cucumbers', 'Peppers', 'Tomatoes', 'Onions', 'Garlic', 'Potatoes']
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
