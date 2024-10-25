1220707c13331f96635bc1df2826d61ab10def8b
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-25 10:09:44 +0100

    fix: remove unwanted print statement

--- 00-beginner/day-05/exercises/01_high-score.py
+++ 00-beginner/day-05/exercises/01_high-score.py
@@ -2,7 +2,6 @@
 student_scores = input("Input a list of student scores ").split()
 for n in range(0, len(student_scores)):
   student_scores[n] = int(student_scores[n])
-print(student_scores)
 # 🚨 Don't change the code above 👆
 
 #Write your code below this row 👇
