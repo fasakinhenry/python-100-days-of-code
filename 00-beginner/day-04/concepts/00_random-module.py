1db787a345bfd995c8264e9c19bf065ecdb3999e
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-25 03:40:04 +0100

    feat: add random float and usage

--- 00-beginner/day-04/concepts/00_random-module.py
+++ 00-beginner/day-04/concepts/00_random-module.py
@@ -4,3 +4,12 @@ import random
 # Print a random integer
 random_integer = random.randint(1, 10)
 print(random_integer)
+
+# Print a random float betweeon 0 and 5
+random_float = random.random() * 5
+print(random_float)
+
+# Usage of the random module
+# Generate a random love score
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")
