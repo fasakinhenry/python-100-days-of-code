93b843166e9d3ce3b68a837ba2ba6ff67c420c5c
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-25 11:29:16 +0100

    feat: add for loop with range concept

---
+++ 00-beginner/day-05/concepts/01_for-and-range.py
@@ -0,0 +1,10 @@
+# use for loop with range to print 1 to 10
+# for number in range(1, 11):
+#     print(number)
+
+# Get the total of consecutive numbers
+total = 0
+for num in range(1, 101):
+    total += num
+
+print(total)
