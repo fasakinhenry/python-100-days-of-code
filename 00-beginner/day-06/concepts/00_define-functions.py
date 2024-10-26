dd6ac14589c77c8193392d33d0a6fc988b5981df
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-26 19:23:36 +0100

    feat: add code for define and call function

---
+++ 00-beginner/day-06/concepts/00_define-functions.py
@@ -0,0 +1,15 @@
+# Defining and calling Python Function
+# Functions is a block of code that performs specific task
+
+# Inbuilt functions
+# These are functions provided by Python to carry out some certain tasdks
+# Examples include Print(), len() functions
+
+# Declaring your function
+def my_function():
+    print("Hello")
+    print("World")
+    print("Bye")
+
+# Calling the function
+my_function()
