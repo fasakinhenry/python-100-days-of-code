643296df4d6b923152a1ebd8d4ff142d795952b0
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-07 20:49:01 +0100

    feat: add code for hurdle 4

---
+++ 00-beginner/day-06/exercises/03_hurdle-loop.py
@@ -0,0 +1,25 @@
+# Test out the code in the Reeborg website
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+# Create a function to turn right
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+# create a function jump for jumping hurdles
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+    
+while not at_goal():
+    if front_is_clear():
+        move()
+    else:
+        jump()
