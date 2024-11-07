fca4dc915f1c01a59541f40a9308a3c7f165ea41
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-07 19:59:54 +0100

    feat: add code for the hurdle 3 challenge

---
+++ 00-beginner/day-06/exercises/01_hurdle-loop.py
@@ -0,0 +1,21 @@
+# Test out the code in the Reeborg website
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+# Create a function to turn right
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+# create a function jump for jumping hurdles
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+while not at_goal():
+    jump()
