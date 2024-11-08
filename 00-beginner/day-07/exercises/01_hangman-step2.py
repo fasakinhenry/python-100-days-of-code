339b815aaff8c5f3ad1c688ac91fb1901b658907
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-08 17:58:41 +0100

    feat: add code for hangman step2

--- 00-beginner/day-07/exercises/01_hangman-step2.py
+++ 00-beginner/day-07/exercises/01_hangman-step2.py
@@ -22,9 +22,10 @@ for _ in range(len(chosen_word)):
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 for letter in chosen_word:
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+        position = chosen_word.index(letter)
+        display[position] = letter
+
 
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+print(display)
