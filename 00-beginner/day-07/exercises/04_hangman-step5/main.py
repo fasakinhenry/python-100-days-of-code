f2876831e823043461e90cdd92c3296e961dc09e
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-09 07:00:37 +0100

    chore: add imports to main file

--- 00-beginner/day-07/exercises/04_hangman-step5/main.py
+++ 00-beginner/day-07/exercises/04_hangman-step5/main.py
@@ -4,13 +4,17 @@ import random
 
 #TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
 #Delete this line: word_list = ["ardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
+import hangman_words
+
+chosen_word = random.choice(hangman_words.word_list)
 word_length = len(chosen_word)
 
 end_of_game = False
 lives = 6
 
 #TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+from hangman_art import logo
+print(logo)
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
