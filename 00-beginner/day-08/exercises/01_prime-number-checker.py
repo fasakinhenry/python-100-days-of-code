e11e7ef0e7692c3516a9463c4af69548c4de6589
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-11 16:37:02 +0100

    feat: update the prime checker program

--- 00-beginner/day-08/exercises/01_prime-number-checker.py
+++ 00-beginner/day-08/exercises/01_prime-number-checker.py
@@ -1,8 +1,11 @@
 #Write your code below this line 👇
-
-
-
-
+# Create a prime number checker
+def prime_checker(number):
+    for divisor in range(2, number):
+        if number % divisor == 0:
+            print("It's not a prime number")
+            return
+    print("It's a prime number")
 
 #Write your code above this line 👆
     
