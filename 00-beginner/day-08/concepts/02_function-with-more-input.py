c8029f495ffd51e1646867a73aac2c26da475b44
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-11 10:51:56 +0100

    feat: learn about positonal vs keyword argument

--- 00-beginner/day-08/concepts/02_function-with-more-input.py
+++ 00-beginner/day-08/concepts/02_function-with-more-input.py
@@ -3,3 +3,9 @@ def greet_with(name, location):
     print(f"Hello {name}")
     print(f"What is it like in {location}?")
     print("How are you today?")
+
+# Call the function with postional arguments
+greet_with("Henry", "London")
+
+# Call the function with keyword arguments
+greet_with(location="London", name="Henry")
