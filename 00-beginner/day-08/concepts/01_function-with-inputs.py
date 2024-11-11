7e96bf320e2c303f0f4fa1c09d6528a83f3935f1
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-11 03:32:02 +0100

    feat: add code for function with inputs

---
+++ 00-beginner/day-08/concepts/01_function-with-inputs.py
@@ -0,0 +1,7 @@
+# Create a function that allows for input
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+# Call the function with arguments
+greet_with_name("Henry")
