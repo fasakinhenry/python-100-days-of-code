549234bc9be2a76a3ca29c84ce3d586d7a61bf57
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-11 13:12:44 +0100

    feat: add template code for prime number checker

---
+++ 00-beginner/day-08/exercises/01_prime-number-checker.py
@@ -0,0 +1,14 @@
+#Write your code below this line 👇
+
+
+
+
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
+
+
+
