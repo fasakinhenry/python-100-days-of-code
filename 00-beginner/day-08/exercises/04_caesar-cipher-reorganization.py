f411a3eea1dad9a3581c07e66091d00d84ea72bc
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-12 05:58:18 +0100

    feat: add the code for caesar cipher refactor

--- 00-beginner/day-08/exercises/04_caesar-cipher-reorganization.py
+++ 00-beginner/day-08/exercises/04_caesar-cipher-reorganization.py
@@ -1,30 +1,21 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
-
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
-    position = alphabet.index(letter)
-    new_position = position + shift_amount
-    cipher_text += alphabet[new_position]
-  print(f"The encoded text is {cipher_text}")
-
-def decrypt(cipher_text, shift_amount):
-  plain_text = ""
-  for letter in cipher_text:
-    position = alphabet.index(letter)
-    new_position = position - shift_amount
-    plain_text += alphabet[new_position]
-  print(f"The decoded text is {plain_text}")
-
-if direction == "encode":
-  encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-  decrypt(cipher_text=text, shift_amount=shift)
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar().
+def caesar(text, shift, direction):
+  result = ""
+  if direction == "decode":
+    shift *= -1
+  for char in text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = (position + shift) % 26
+      result += alphabet[new_position]
+    else:
+      result += char
+  print(f"The {direction}d text is {result}")
 
 #TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+caesar(text, shift, direction)
