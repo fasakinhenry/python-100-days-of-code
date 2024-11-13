fbe6234cc7f87baea8a33d2f5955639fb186a2a7
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-13 20:09:06 +0100

    feat: add template code and info for grading program

---
+++ 00-beginner/day-09/exercises/00_grading-program.py
@@ -0,0 +1,22 @@
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+# TODO-1: Create an empty dictionary called student_grades.
+
+
+# TODO-2: Write your code below to add the grades to student_grades.👇
+
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
+
+
+
+
+
