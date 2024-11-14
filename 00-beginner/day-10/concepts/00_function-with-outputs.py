c357269df318648efe96c0114207599ea1824aa3
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-14 11:44:02 +0100

    feat: add code for function with outputs
    contains teaching about return keyword

---
+++ 00-beginner/day-10/concepts/00_function-with-outputs.py
@@ -0,0 +1,5 @@
+# Functions with output
+def format_name(f_name, l_name):
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return f"{formatted_f_name} {formatted_l_name}"
