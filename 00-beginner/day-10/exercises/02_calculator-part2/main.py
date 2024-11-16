92bacfa1b58d80abf2ede5a17285db1630509215
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-16 17:45:38 +0100

    feat: upate calculator to have restart mechanism

--- 00-beginner/day-10/exercises/02_calculator-part2/main.py
+++ 00-beginner/day-10/exercises/02_calculator-part2/main.py
@@ -22,8 +22,6 @@ from art import logo
 # Print the logo
 print(logo)
 
-
-
 # create a dictionary to store the operations
 operations = {
     "+": add,
@@ -32,33 +30,40 @@ operations = {
     "/": divide
 }
 
-# Create variables to store the user numbers
-num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second number?: "))
+# create the calculator function
+def calculator():
+    """This function will perform the calculator operations"""
+    # Create variables to store the user numbers
+    num1 = int(input("What's the first number?: "))
+    num2 = int(input("What's the second number?: "))
 
-# Create a loop to ask the user for the operation
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
+    # Create a loop to ask the user for the operation
+    for symbol in operations:
+        print(symbol)
+    operation_symbol = input("Pick an operation from the line above: ")
 
-# Call the operation function from the operations dictionary
-calculation_function = operations[operation_symbol]
-result = calculation_function(num1, num2)
+    # Call the operation function from the operations dictionary
+    calculation_function = operations[operation_symbol]
+    result = calculation_function(num1, num2)
 
-# Print the result
-print(f"{num1} {operation_symbol} {num2} = {result}")
+    # Print the result
+    print(f"{num1} {operation_symbol} {num2} = {result}")
 
-# Ensure continuation of the calculator
-continue_calculating = True
-while continue_calculating:
-    user_choice = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
-    if user_choice == "y":
-        num1 = result
-        operation_symbol = input("Pick an operation: ")
-        num2 = int(input("What's the next number?: "))
-        calculation_function = operations[operation_symbol]
-        result = calculation_function(num1, num2)
-        print(f"{num1} {operation_symbol} {num2} = {result}")
-    else:
-        continue_calculating = False
-        print("Goodbye")
+    # Ensure continuation of the calculator
+    continue_calculating = True
+    while continue_calculating:
+        user_choice = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+        if user_choice == "y":
+            num1 = result
+            operation_symbol = input("Pick an operation: ")
+            num2 = int(input("What's the next number?: "))
+            calculation_function = operations[operation_symbol]
+            result = calculation_function(num1, num2)
+            print(f"{num1} {operation_symbol} {num2} = {result}")
+        else:
+            continue_calculating = False
+            # Call the calculator recursively
+            calculator()
+
+# call the calculator function
+calculator()
