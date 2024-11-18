198ed3160e0d727511cd797e6a49ec562bd82d25
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-18 15:58:03 +0100

    fix: fix bug in debugging fizzbuzz

--- 00-beginner/day-13/exercises/02_debug-fizzbuzz.py
+++ 00-beginner/day-13/exercises/02_debug-fizzbuzz.py
@@ -1,9 +1,26 @@
+# for number in range(1, 101):
+#   if number % 3 == 0 or number % 5 == 0:
+#     print("FizzBuzz")
+#   if number % 3 == 0:
+#     print("Fizz")
+#   if number % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print([number])
+
+# Solution
+# This is the solution
+# We use and operator to check if the number is divisible by 5 and 3 instead of using or
 for number in range(1, 101):
-  if number % 3 == 0 or number % 5 == 0:
+  #or should be and  
+  if number % 3 == 0 and number % 5 == 0:
     print("FizzBuzz")
-  if number % 3 == 0:
+  #if should be elif
+  elif number % 3 == 0:
     print("Fizz")
-  if number % 5 == 0:
+  #if should be elif
+  elif number % 5 == 0:
     print("Buzz")
   else:
-    print([number])
+    #This prints a list of number instead of just the number.
+    print(number)
