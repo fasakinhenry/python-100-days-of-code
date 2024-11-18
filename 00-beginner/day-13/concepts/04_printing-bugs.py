3d7cb0213ec51a0cb6b98cabd64a49040e5eb9a1
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-18 07:38:56 +0100

    feat: add the code for printing bugs error

--- 00-beginner/day-13/concepts/04_printing-bugs.py
+++ 00-beginner/day-13/concepts/04_printing-bugs.py
@@ -1,30 +1,5 @@
 ############DEBUGGING#####################
 
-# # Describe Problem
-# def my_function():
-#   for i in range(1, 20):
-#     if i == 20:
-#       print("You got it")
-# my_function()
-
-# # Reproduce the Bug
-# from random import randint
-# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
-# print(dice_imgs[dice_num])
-
-# # Play Computer
-# year = int(input("What's your year of birth?"))
-# if year > 1980 and year < 1994:
-#   print("You are a millenial.")
-# elif year > 1994:
-#   print("You are a Gen Z.")
-
-# # Fix the Errors
-# age = input("How old are you?")
-# if age > 18:
-# print("You can drive at age {age}.")
-
 # #Print is Your Friend
 # pages = 0
 # word_per_page = 0
@@ -33,12 +8,16 @@
 # total_words = pages * word_per_page
 # print(total_words)
 
-# #Use a Debugger
-# def mutate(a_list):
-#   b_list = []
-#   for item in a_list:
-#     new_item = item * 2
-#   b_list.append(new_item)
-#   print(b_list)
-
-# mutate([1,2,3,5,8,13])
+# Solution
+# The problem is that we are using the equal to(==) operator for word_per_page instead of assignment(=) operator
+# To fix the issue, we will print the variables pages and word per page
+# We then change the equal to assignment(one equal sign)
+# Printing the variables made debugging this code easier
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+print(f"Pages = {pages}")
+print(f"Word_per_page = {word_per_page}")
+total_words = pages * word_per_page
+print(total_words)
