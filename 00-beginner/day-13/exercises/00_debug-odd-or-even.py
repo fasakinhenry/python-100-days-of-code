f495f6eafd95e78cf9fbcb893fbc8b01280a2f52
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-18 07:58:23 +0100

    :docs: add the instruction for debug odd or even exercise

---
+++ 00-beginner/day-13/exercises/00_debug-odd-or-even.py
@@ -0,0 +1,11 @@
+# Debug Odd or Even
+Instructions
+Read this the code in main.py
+Spot the problems 🐞.
+Modify the code to fix the program.
+Fix the code so that it works and passes the tests when you submit.
+
+Hint
+Review the previous lesson and go through the 10 steps to tackle these debugging problems.
+Solution
+https://repl.it/@appbrewery/day-13-1-solution
