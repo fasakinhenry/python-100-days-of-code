15fba3925180f56ffa21764cc649420374057665
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-18 15:43:24 +0100

    fix: fix the bug in leap year

--- 00-beginner/day-13/exercises/01_debug-leap-year.py
+++ 00-beginner/day-13/exercises/01_debug-leap-year.py
@@ -1,4 +1,20 @@
-year = input("Which year do you want to check?")
+# year = input("Which year do you want to check?")
+
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year.")
+#     else:
+#       print("Not leap year.")
+#   else:
+#     print("Leap year.")
+# else:
+#   print("Not leap year.")
+
+# Solution
+# Problem: The input from the user should be converted into int
+# Add a type conversion to the first line of the code to convert input to integer
+year = int(input("Which year do you want to check? "))
 
 if year % 4 == 0:
   if year % 100 == 0:
@@ -10,4 +26,3 @@ if year % 4 == 0:
     print("Leap year.")
 else:
   print("Not leap year.")
-  
