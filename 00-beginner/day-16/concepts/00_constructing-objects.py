80d601776adf0de557ab7889e100d3b16f8667fc
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-20 03:53:35 +0100

    feat: update constructing objects file

--- 00-beginner/day-16/concepts/00_constructing-objects.py
+++ 00-beginner/day-16/concepts/00_constructing-objects.py
@@ -1,5 +1,24 @@
 # Get started with the Turtle class
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 # Create a new object from the turtle class
 timmy = Turtle()
+
+# Print the new created turtle object
+print(timmy)
+# Change the shape of the turtle object
+timmy.shape("turtle")
+# Change the color of the turtle object
+timmy.color("coral")
+
+# Move the turtle forward by 100 paces
+timmy.forward(100)
+
+# Create a new screen object
+my_screen = Screen()
+
+# Print the canvas height of the screen
+print(my_screen.canvheight)
+
+# Show up screen until it is clicked
+my_screen.exitonclick()
