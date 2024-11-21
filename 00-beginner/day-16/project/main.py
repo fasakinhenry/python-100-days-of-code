5245413a0e99e9054059e2a6d917bcfd9487d856
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-21 18:29:07 +0100

    feat: add code for OOP coffee machine version

--- 00-beginner/day-16/project/main.py
+++ 00-beginner/day-16/project/main.py
@@ -1,3 +1,32 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+# Get the list of items from the Menu class
+menu = Menu()
+
+# Create the coffee maker object
+coffee_maker = CoffeeMaker()
+
+# Create the money machine object
+moneyMachine = MoneyMachine()
+
+is_on = True
+
+while is_on:
+    # Print the greeting message
+    choice = input(f"What would you like? ({menu.get_items()}): ")
+
+    # Check inputs given by the user
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        moneyMachine.report()
+    elif menu.find_drink(choice) != None:
+        # Get the drink from the menu
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            moneyMachine.make_payment(drink.cost)
+            coffee_maker.make_coffee(drink)
+
