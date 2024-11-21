c92abd8a0aeb46b28cc944fde2a5098ce7646fd9
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-21 18:36:54 +0100

    refactor: make coffe machine code follow PEP8 standards

--- 00-beginner/day-16/project/main.py
+++ 00-beginner/day-16/project/main.py
@@ -4,12 +4,10 @@ from money_machine import MoneyMachine
 
 # Get the list of items from the Menu class
 menu = Menu()
-
 # Create the coffee maker object
 coffee_maker = CoffeeMaker()
-
 # Create the money machine object
-moneyMachine = MoneyMachine()
+money_machine = MoneyMachine()
 
 is_on = True
 
@@ -22,11 +20,11 @@ while is_on:
         is_on = False
     elif choice == "report":
         coffee_maker.report()
-        moneyMachine.report()
-    elif menu.find_drink(choice) != None:
+        money_machine.report()
+    else:
         # Get the drink from the menu
         drink = menu.find_drink(choice)
         if coffee_maker.is_resource_sufficient(drink):
-            moneyMachine.make_payment(drink.cost)
+            money_machine.make_payment(drink.cost)
             coffee_maker.make_coffee(drink)
 
