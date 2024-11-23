b5daabee288800b44728ed07c755973ff0fd3f88
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-23 23:01:52 +0100

    feat: add method to check if quiz questions are present

--- 00-beginner/day-17/project/quiz_brain.py
+++ 00-beginner/day-17/project/quiz_brain.py
@@ -3,6 +3,11 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = q_list
 
+    def still_has_questions(self):
+        if self.question_number < len(self.question_list)
+            return True
+        return False
+
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
