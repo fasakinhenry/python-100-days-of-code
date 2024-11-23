a066bc41643d29a07abac75e4e4e0bb560f8e9df
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-23 23:51:53 +0100

    chore: update parameters for accessing question bank

--- 00-beginner/day-17/project/main.py
+++ 00-beginner/day-17/project/main.py
@@ -6,7 +6,7 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for question in question_data:
-    question_bank.append(Question(question["text"], question["answer"]))
+    question_bank.append(Question(question["question"], question["correct_answer"]))
 
 quiz_brain = QuizBrain(question_bank)
 
