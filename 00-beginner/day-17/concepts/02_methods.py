4810ccf937b071dcf15dabb1d37fc5432913174b
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-23 22:03:23 +0100

    feat: add the code to work with methods

---
+++ 00-beginner/day-17/concepts/02_methods.py
@@ -0,0 +1,20 @@
+# Create a user class
+class User:
+    def __init__(self, user_id, username) -> None:
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+user_1 = User("001", "Henry")
+user_2 = User("002", "Angela")
+
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
