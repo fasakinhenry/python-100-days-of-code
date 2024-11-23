39967627c2bc27bb42533476d69d88070c9d6c01
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-23 23:34:57 +0100

    feat: add code to print final quiz result

--- 00-beginner/day-17/project/main.py
+++ 00-beginner/day-17/project/main.py
@@ -12,3 +12,7 @@ quiz_brain = QuizBrain(question_bank)
 
 while quiz_brain.still_has_questions():
     quiz_brain.next_question()
+
+# Print final statements
+print("You've completed the quiz")
+print(f"Your final score was: {quiz_brain.score}/{quiz_brain.question_number}")
