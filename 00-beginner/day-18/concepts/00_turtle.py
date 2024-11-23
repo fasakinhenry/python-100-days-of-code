c85ff76f1088bd5b10502baf4b351fa42e14ef95
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-24 00:11:52 +0100

    feat: add code for turtle concept

---
+++ 00-beginner/day-18/concepts/00_turtle.py
@@ -0,0 +1,10 @@
+# Import the turtle and screen class from turtle
+from turtle import Turtle, Screen
+
+# create a new turtle object
+timmy = Turtle()
+timmy.color("Blue")
+timmy.shape("triangle")
+
+screen = Screen()
+screen.exitonclick()
