7a10f82eadb6c83a120bb9e9ea2dba52cf05efbe
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-25 23:14:23 +0100

    feat: add color change functionality to drawing shapes

--- 00-beginner/day-18/exercises/02_draw-polygons.py
+++ 00-beginner/day-18/exercises/02_draw-polygons.py
@@ -1,13 +1,19 @@
 # import the turtle and screen class
 from turtle import Turtle, Screen
+import random
+
+# Create a turtle colors array
+colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 
 turtle = Turtle()
 
 # Create a loop to draw the polygons
-for deg in range(3, 10):
+for deg in range(3, 11):
     for move in range(deg):
         turtle.forward(100)
         turtle.right(360/deg)
+    # change the color of the pen after each shape
+    turtle.color(random.choice(colors))
 
 
 screen = Screen()
