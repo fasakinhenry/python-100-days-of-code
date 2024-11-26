fb086f7d3ea393e3ed487eb30be9bd73071ae32c
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-27 00:28:31 +0100

    refactor: create an attribute for the head snake

--- 00-beginner/day-20/concepts/02_creating-snake-class/snake.py
+++ 00-beginner/day-20/concepts/02_creating-snake-class/snake.py
@@ -12,6 +12,7 @@ class Snake:
         self.color = "white"
         self.initial_position = STARTING_POSITION
         self.create_snake()
+        self.head = self.segments[0]
     
     def create_snake(self):
         for position in range(3):
@@ -32,16 +33,16 @@ class Snake:
         self.segments[0].forward(MOVE_DISTANCE)
     
     def up(self):
-        self.segments[0].setheading(90)
+        self.head.setheading(90)
 
     def down(self):
-        self.segments[0].setheading(270)
+        self.head.setheading(270)
 
     def left(self):
-        self.segments[0].setheading(180)
+        self.head.setheading(180)
 
     def right(self):
-        self.segments[0].setheading(0)
+        self.head.setheading(0)
 
 
 
