df8de4d61ea9f841af2292e14c7234800535cf0c
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-26 01:42:09 +0100

    refactor: update the code for the random walk

--- 00-beginner/day-18/exercises/03_random-walk.py
+++ 00-beginner/day-18/exercises/03_random-walk.py
@@ -6,12 +6,13 @@ turtle = Turtle()
 
 # create the colors array
 colors = ["cornflower blue", "dark orchid", "indian red", "deep sky blue", "light sea green", "wheat", "slate gray", "sea green"]
+directions = [0, 90, 180, 270]
 
 # Implement the turtle Random Walk
 turtle.speed("fastest")
 
 for _ in range(200):
-    turtle.setheading(random.choice([0, 90, 180, 270]))
+    turtle.setheading(random.choice(directions))
     turtle.color(random.choice(colors))
     turtle.pensize(10)
     turtle.forward(30)
