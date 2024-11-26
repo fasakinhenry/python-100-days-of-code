c4c13fd8ffc09dd1ccd798f800756b43433d134d
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-26 12:52:44 +0100

    feat: add the code for hirst painting project

--- 00-beginner/day-18/project/main.py
+++ 00-beginner/day-18/project/main.py
@@ -1,17 +1,37 @@
-###This code will not work in repl.it as there is no access to the colorgram package here.###
-##We talk about this in the video tutorials##
-import colorgram
+# Create the hirst painting using turtle
+import turtle as t
+import random
 
-rgb_colors = []
-colors = colorgram.extract('00-beginner/day-18/project/image.jpg', 30)
+turtle = t.Turtle()
 
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r, g, b)
-    rgb_colors.append(new_color)
+# Change the color mode using the turtle class
+t.colormode(255)
 
-print(rgb_colors)
+# create function to generate random colors
+def random_color(color_list):
+    color = random.choice(color_list)
+    return color
 
+# create the colors array
 color_list = [(202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+
+turtle.speed("fastest")
+
+# Set the starting position
+turtle.penup()
+turtle.setpos(-250, -250)
+turtle.pendown()
+
+for _ in range(10):
+    for _ in range(10):
+        turtle.dot(20, random_color(color_list))
+        turtle.penup()
+        turtle.forward(50)
+        turtle.pendown()
+    turtle.penup()
+    turtle.setpos(-250, turtle.ycor() + 50)
+    turtle.pendown()
+
+
+screen = t.Screen()
+screen.exitonclick()
