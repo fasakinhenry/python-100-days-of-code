ebe7119060d6dcd996598a8b34f694501489993d
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-28 20:48:47 +0100

    feat: add methods to increase score in scoreboard

--- 00-beginner/day-22/project/scoreboard.py
+++ 00-beginner/day-22/project/scoreboard.py
@@ -8,7 +8,17 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.l_score = 0
         self.r_score = 0
+
+    def update_score(self):
+        self.clear()
         self.goto(-100, 180)
         self.write(self.l_score, align="center", font=("Poppins", 80, "normal"))
         self.goto(100, 180)
         self.write(self.r_score, align="center", font=("Poppins", 80, "normal"))
+
+    def l_point(self):
+        self.l_score += 1
+    
+    def r_point(self):
+        self.r_score += 1
+
