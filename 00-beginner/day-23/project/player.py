18e6ac04e9b6bd99880e155e45f379ff0a92748f
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-28 21:37:43 +0100

    feat: create the player class

--- 00-beginner/day-23/project/player.py
+++ 00-beginner/day-23/project/player.py
@@ -1,7 +1,18 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
+class Player(Turtle):
+    def __init__(self) -> None:
+        super().__init__()
+        self.shape("turtle")
+        self.starting_position = STARTING_POSITION
+        self.penup()
+        self.goto(self.starting_position)
+        self.setheading(90)
 
-class Player:
-    pass
+    def move(self):
+        self.new_y = self.ycor() + 20
+        self.goto(self.xcor(), self.new_y)
