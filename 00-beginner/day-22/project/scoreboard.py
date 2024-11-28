dc0ade8475091629225229617ea472b5bc984e7d
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-28 20:42:44 +0100

    feat: create the scoreboard class to manage scores

---
+++ 00-beginner/day-22/project/scoreboard.py
@@ -0,0 +1,12 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self) -> None:
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
