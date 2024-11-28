c26794047392c0ba9890e4fd6511a3189d3fec29
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-28 09:03:19 +0100

    ref: refactor snake collision technique

--- 00-beginner/day-21/project/main.py
+++ 00-beginner/day-21/project/main.py
@@ -41,4 +41,10 @@ while game_is_on:
         game_is_on = False
         score_board.game_over()
 
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            score_board.game_over()
+
 screen.exitonclick()
