aaef2283f771e42a282acb7f56927cc69c34f30f
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-28 20:12:00 +0100

    feat: add bouncing capability to ball

--- 00-beginner/day-22/project/ball.py
+++ 00-beginner/day-22/project/ball.py
@@ -6,8 +6,15 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        self.new_x = self.xcor() + 10
-        self.new_y = self.ycor() + 10
+        self.new_x = self.xcor() + self.x_move
+        self.new_y = self.ycor() + self.y_move
         self.goto(self.new_x, self.new_y)
+        self.bounce()
+    
+    def bounce(self):
+        if self.ycor() > 280 or self.ycor() > 280:
+            self.y_move *= -1
