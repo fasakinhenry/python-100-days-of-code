c4188df3e16023f1ea52bf2efc33f5a257f3b9b9
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-29 00:25:18 +0100

    feat: add method to reset score and highscore

--- 00-beginner/day-24/project/scoreboard.py
+++ 00-beginner/day-24/project/scoreboard.py
@@ -9,6 +9,7 @@ class ScoreBoard(Turtle):
     def __init__(self) -> None:
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -16,7 +17,14 @@ class ScoreBoard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        self.clear()
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
     
     def game_over(self):
         self.goto(0, 0)
