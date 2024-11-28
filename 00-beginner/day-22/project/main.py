4fab47becd1f2ebfbc03800f93afcc941a5a29c7
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-28 19:50:51 +0100

    feat: add the main file for pong game

--- 00-beginner/day-22/project/main.py
+++ 00-beginner/day-22/project/main.py
@@ -1,10 +1,26 @@
 from turtle import Screen
+from paddle import Paddle
 
 # create an object of the screen class
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game - Famous arcade game")
+screen.tracer(0)
+
+left_paddle = Paddle((-350, 0))
+right_paddle = Paddle((350, 0))
+
+screen.listen()
+screen.onkey(right_paddle.move_up,"Up")
+screen.onkey(right_paddle.move_down,"Down")
+screen.onkey(left_paddle.move_up,"w")
+screen.onkey(left_paddle.move_down,"s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
 
 # Ensure screen exits when it is clicked
 screen.exitonclick()
