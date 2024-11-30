2c58daeaf5ec625cbf31cf6ea4b1a2d17d2b7e09
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-30 23:11:44 +0100

    fix: add hints for solving challenge

--- 00-beginner/day-24/project/main.py
+++ 00-beginner/day-24/project/main.py
@@ -4,5 +4,7 @@
 #Save the letters in the folder "ReadyToSend".
     
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+
+with open("./input/Names/invited_names.txt", mode="r") as file:
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
