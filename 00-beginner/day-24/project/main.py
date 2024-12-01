10096eb460f7ff55c27cfd7d1254f6d94df62f91
Fasakin Henry <fasakinhenry@gmail.com>
2024-12-01 22:28:02 +0100

    docs: add comments in the code

--- 00-beginner/day-24/project/main.py
+++ 00-beginner/day-24/project/main.py
@@ -7,24 +7,17 @@ import os
 script_dir = os.path.dirname(__file__)
 file_path = os.path.join(script_dir, "./input/Names/invited_names.txt")
 
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
 with open(file_path, mode="r") as file:
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
     # Get each names without the newline character
     names = file.readlines()
     start_path = os.path.join(script_dir, "./input/Letters/starting_letter.txt")
     for name in names:
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
         with open(start_path, mode="r") as file:
             letter = file.read()
             final_path = os.path.join(script_dir, f"./output/ReadyToSend/letter_for_{name.strip()}.txt")
             with open(final_path, mode="w") as file:
                 file.write(letter.replace("[name]", name.strip()))
 
-# replace the name in the letter
-# with open("./input/Letters/starting_letter.txt", mode="r") as file:
-#     letter = file.read()
-#     for name in names:
-#         with open(f"./output/ReadyToSend/letter_for_{name.strip()}.txt", mode="w") as file:
-#             file.write(letter.replace("[name]", name.strip()))
-
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
