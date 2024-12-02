8524e220a04767febd03d0e3eca3d48711054254
Fasakin Henry <fasakinhenry@gmail.com>
2024-12-02 23:49:30 +0100

    docs: add comment to code

--- 00-beginner/day-25/concepts/01_reading-csv-using-csv-module.py
+++ 00-beginner/day-25/concepts/01_reading-csv-using-csv-module.py
@@ -1,9 +1,10 @@
 import os
+# import the csv module used to work with CSV files
+import csv
 
 script_dir = os.path.dirname(__file__)
 file_path = os.path.join(script_dir, "weather-data.csv")
 
-import csv
 
 with open(file_path) as data_file:
     data = csv.reader(data_file)
