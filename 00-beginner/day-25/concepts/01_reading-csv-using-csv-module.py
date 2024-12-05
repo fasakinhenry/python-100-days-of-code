9a638ba3f23dd1f46f5d5e5f0b81539c656b7dce
Fasakin Henry <fasakinhenry@gmail.com>
2024-12-05 17:07:02 +0100

    docs: document code

--- 00-beginner/day-25/concepts/01_reading-csv-using-csv-module.py
+++ 00-beginner/day-25/concepts/01_reading-csv-using-csv-module.py
@@ -5,7 +5,7 @@ import csv
 script_dir = os.path.dirname(__file__)
 file_path = os.path.join(script_dir, "weather-data.csv")
 
-
+# create an array for all temperatures in weather.csv
 with open(file_path) as data_file:
     data = csv.reader(data_file)
     temperatures = []
